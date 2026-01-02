9bba57a17145e04e840b9e8e4af6eb0ef3459a6d
河本佑景 <uk0706smile@gmail.com>
2026-01-02 13:39:18 +0900

    Dockerでbackendをコンテナ化しcomposeで起動管理できる環境構築

--- backend/app.py
+++ backend/app.py
@@ -62,4 +62,4 @@ bcrypt.init_app(app)
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True, port=5001)
+    app.run(host="0.0.0.0", port=5001, debug=True)
